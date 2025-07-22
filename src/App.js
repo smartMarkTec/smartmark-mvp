d249dbce4f3be3b23a8a11a67d71c6306febdab5
Knowles <knowleshouse@Knowless-Air.attlocal.net>
2025-07-22 14:53:46 -0500

    fix: revert App.js to original routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import FormPage from "./pages/FormPage";
 import CampaignSetup from "./pages/CampaignSetup";
-import Login from "./pages/Login";
+import Login from "./pages/Login"; // <--- ADD THIS LINE
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-black flex justify-center items-start p-2 sm:p-4">
-      <div className="w-full max-w-md">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="/setup" element={<CampaignSetup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/form" element={<FormPage />} />
+      <Route path="/setup" element={<CampaignSetup />} />
+      <Route path="/login" element={<Login />} /> {/* <--- ADD THIS LINE */}
+    </Routes>
   );
 }
 
