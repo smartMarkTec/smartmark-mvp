829d234e27f01a5dc8bca9a7cf4b942bcffebd88
Knowles <knowleshouse@Knowless-Air.attlocal.net>
2025-07-22 14:44:12 -0500

    feat: add responsive container for mobile layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import FormPage from "./pages/FormPage";
 import CampaignSetup from "./pages/CampaignSetup";
-import Login from "./pages/Login"; // <--- ADD THIS LINE
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/form" element={<FormPage />} />
-      <Route path="/setup" element={<CampaignSetup />} />
-      <Route path="/login" element={<Login />} /> {/* <--- ADD THIS LINE */}
-    </Routes>
+    <div className="min-h-screen bg-white text-black flex justify-center items-start p-2 sm:p-4">
+      <div className="w-full max-w-md">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/form" element={<FormPage />} />
+          <Route path="/setup" element={<CampaignSetup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
