fbca81e0759b429e07310baf38fff3c91f4036f1
Knowles <knowleshouse@Knowless-Air.attlocal.net>
2025-07-23 17:11:36 -0500

    fix: add NotFound route for production SPA stability

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import FormPage from "./pages/FormPage";
 import CampaignSetup from "./pages/CampaignSetup";
 import Login from "./pages/Login";
 
-// You can expand with a NotFound page if you wish
-// import NotFound from "./pages/NotFound";
+function NotFound() {
+  return (
+    <div style={{ color: "#fff", background: "#232529", minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <a href="/" style={{ color: "#1ec885", marginTop: 20, fontWeight: 700 }}>Go to Home</a>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -16,9 +22,7 @@ function App() {
         <Route path="/form" element={<FormPage />} />
         <Route path="/setup" element={<CampaignSetup />} />
         <Route path="/login" element={<Login />} />
-        {/* 
-        <Route path="*" element={<NotFound />} /> 
-        */}
+        <Route path="*" element={<NotFound />} />   {/* CATCH-ALL */}
       </Routes>
     </Router>
   );
