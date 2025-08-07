b08569f7740b9850726cdeae928fcb988c0bb7c7
Knowles <knowleshouse@Knowless-MacBook-Air.local>
2025-08-07 01:44:48 -0500

    fix: back button always routes to FormPage

--- src/pages/CampaignSetup.js
+++ src/pages/CampaignSetup.js
@@ -480,7 +480,7 @@ const handleLaunch = async () => {
         }}
       >
         <button
-          onClick={() => navigate(-1)}
+           onClick={() => navigate(-1)}
           style={{
             background: "#202824e0",
             color: "#fff",
