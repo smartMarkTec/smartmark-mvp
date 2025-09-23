173935d9976bcb95649a46af04d070a3e9deb770
Knowles <knowleshouse@Knowless-MacBook-Air.local>
2025-09-22 19:09:16 -0500

    update

--- server/server.js
+++ server/server.js
@@ -60,6 +60,7 @@ app.use((req, res, next) => {
 
 app.set('trust proxy', 1);
 app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // --- Serve generated assets for AI overlays ---
